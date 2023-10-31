2ee6932fc90bdd9575435ddc4f8fffb2c35f8e84
pumavi <145634082+pumavi@users.noreply.github.com>
2023-10-31 13:31:58 +0530

    Delete score percentage.py

--- score percentage.py
+++
@@ -1,8 +0,0 @@
-score=int(input('score:'))
-if(score>=70):
-    name=str(input('what is your name:'))
-    department=str(input('what is your department:'))
-    location=str(input('whats is your location:'))
-    print('you are eligible')
-else:
-    print('you are not eligible')
